7d4ec6943befd2a641f39a84c9cdd92f5e5c28b8
sisiraskumar <sisiraskumarmadayi@gmail.com>
2025-09-24 20:52:31 +0530

    Update siii.py

--- siii.py
+++ siii.py
@@ -2,6 +2,6 @@ name=str(input("enter your name"))
 age=int(input("enter your age"))
 gender=str(input("enter your gender"))
 
-print(f"hello welcome ,{name} your age is{age} and your gender is{gender}")
+print(f"hello welcome ,{name} your age is{age} and your gender is{gender}, all the best")
 
 
